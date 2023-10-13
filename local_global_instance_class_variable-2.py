ba5fb0cc3f459476d7010700c82460f2c746bac1
Sandipta Pattanayak <sandipta.vi.pattanayak@gmail.com>
2023-10-14 01:08:30 +0530

    Day3

---
+++ local_global_instance_class_variable-2.py
@@ -0,0 +1,17 @@
+x=10
+class Test:
+	x=5
+	def __init__(self):
+		self.x=4
+	def show(self):
+		x=6
+		print("local variable",x)
+		print("global variable=",globals()['x'])
+		print("Class variable=",Test.x)
+		print("Instance variable",self.x)
+		print(globals()['x'],Test.x,self.x,x)	
+t=Test()
+t1=Test()
+t2=Test()
+t.show()
+t1.show()
--- local_global_instance_class_variable.py
+++ local_global_instance_class_variable.py
@@ -1,23 +1,17 @@
+x=10
 class Test:
-  x=1
-
+  x=5
   def __init__(self):
-    self.y=1
-    Test.x=Test.x+1
-    self.y=self.y+1
-
+    self.x=4
   def show(self):
-    print(Test.x,self.y)
-
+    x=6
+    print("local variable",x)
+    print("global variable=",globals()['x'])
+    print("Class variable=",Test.x)
+    print("Instance variable",self.x)
+    print(globals()['x'],Test.x,self.x,x) 
 t=Test()
 t1=Test()
 t2=Test()
-
-Test.x=6
 t.show()
-t1.show()     
-t2.show() 
-print(t.__dict__)
-print(t1.__dict__)
-print(t2.__dict__)
-print(Test.__dict__)
+t1.show()
